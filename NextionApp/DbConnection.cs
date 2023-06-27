fe33c3d1c12cbf3102e8a9cab60c4c150da56b01
EnoughSem <sema7018@gmail.com>
2023-06-27 19:05:25 +0300

    n1

---
+++ NextionApp/DbConnection.cs
@@ -0,0 +1,50 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NextionApp
+{
+    class DbConnection
+    {
+        static NpgsqlConnection connection = new NpgsqlConnection("Server = mamont-server.ru; Port = 5432; Database = ostankino; User Id = daniel;Password = testStudent123;");
+        
+
+        public static DateTime[] GetDate()
+        {
+            NpgsqlCommand command = new NpgsqlCommand("SELECT \"Time_start\", \"Time_end\" FROM \"Schedule\" ORDER BY \"Time_start\" DESC LIMIT 1", connection);
+            try
+            {
+                connection.Open();
+                NpgsqlDataReader dataReader = command.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    var dateTimeStart = dataReader.GetDateTime(0);
+                    //Console.WriteLine(dateTimeStart);
+                    var dateTimeEnd = dataReader.GetDateTime(1);
+                    //Console.WriteLine(dateTimeEnd);
+                    connection.Close();
+                    return new DateTime[] { dateTimeStart, dateTimeEnd };
+                }
+                               
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message.ToString());
+                connection.Close();
+            }
+
+            return new DateTime[0];
+        }
+
+        public static void AddTime(DateTime dateTimeStart, DateTime dateTimeEnd)
+        {
+            NpgsqlCommand command = new NpgsqlCommand("INSERT INTO \"Schedule\" VALUES (uuid_generate_v4(), 15, TIMESTAMP WITH TIME ZONE \'"+dateTimeStart+ "\', TIMESTAMP WITH TIME ZONE \'"+dateTimeEnd+"\')", connection);
+            connection.Open();
+            command.ExecuteNonQuery();
+            connection.Close();
+        }
+    }
+}
--- NextionApp/Program.cs
+++ NextionApp/Program.cs
@@ -1,39 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO.Ports;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
 
 namespace NextionApp
 {
     class Program
     {
-         
+     
         static void Main(string[] args)
         {
-            SerialPort serialPort = new SerialPort();
-
-            var hour = DateTime.Now.ToString("HH");
-            var min = DateTime.Now.ToString("mm");
-            var sec = DateTime.Now.ToString("ss");
-
-            serialPort.PortName = "COM3";
-            serialPort.BaudRate = 9600;
-            serialPort.DataReceived += SerialPort_DataReceived;
-            serialPort.Open();
-
-            byte[] bytesEnd = new byte[] { 0xff, 0xff, 0xff};
-
-            serialPort.Write("rtc3="+hour);
-            serialPort.Write(bytesEnd,0, bytesEnd.Length);
-
-            serialPort.Write("rtc4=" + min);
-            serialPort.Write(bytesEnd, 0, bytesEnd.Length);
-
-            serialPort.Write("rtc5=" + sec);
-            serialPort.Write(bytesEnd, 0, bytesEnd.Length);
-
+            serialPort();
             Console.ReadLine();
         }
 
@@ -45,24 +23,181 @@ namespace NextionApp
                 string command = _serialPort.ReadLine();
                 if (command.Contains("pass"))
                 {
-                    byte[] bytesEnd = new byte[] { 0xff, 0xff, 0xff };
-                    string subCommand = command.Substring(command.IndexOf("pass")+4, 4);
-                    if (subCommand == "1234")
-                    {
-                        _serialPort.Write("page MainPage");
-                        _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
-
-                    }
-                    else
-                    {
-                        _serialPort.Write("t0.txt= "+ "");
-                        _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
-                       
-                    }
+                    checkPassword(_serialPort, command);
                 }
-                
+                if (command.Contains("time"))
+                {
+                    getTime(_serialPort);
+                }
+                if (command.Contains("date"))
+                {
+                    dateReserv(_serialPort);
+                }
+                if (command.Contains("addDate"))
+                {
+                    addData(_serialPort, command);
+                }
+                if (command.Contains("addTime"))
+                {
+                    addTime(_serialPort, command);
+                }
             }
         }
 
+        private static void serialPort()
+        {
+                SerialPort serialPort = new SerialPort();        
+                serialPort.PortName = "COM3";
+                serialPort.BaudRate = 9600;
+                serialPort.DataReceived += SerialPort_DataReceived;
+                serialPort.Open();
+                getTime(serialPort);   
+        }
+        
+        private static void getTime(SerialPort _serialPort)
+        {
+            var hour = DateTime.Now.ToString("HH");
+            var min = DateTime.Now.ToString("mm");
+            var sec = DateTime.Now.ToString("ss");
+
+            byte[] bytesEnd = new byte[] { 0xff, 0xff, 0xff };
+
+            _serialPort.Write("rtc3=" + hour);
+            _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+
+            _serialPort.Write("rtc4=" + min);
+            _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+
+            _serialPort.Write("rtc5=" + sec);
+            _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+        }
+
+        private static void checkPassword(SerialPort _serialPort, string command)
+        {
+            byte[] bytesEnd = new byte[] { 0xff, 0xff, 0xff };
+            string subCommand = command.Substring(command.IndexOf("pass") + 4, 4);
+            if (subCommand == "1234")
+            {
+                _serialPort.Write("page MainPage");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                dateReserv(_serialPort);
+            }
+            else
+            {
+                _serialPort.Write("t0.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t1.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t2.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t3.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+            }
+        }
+
+
+        private static void dateReserv(SerialPort _serialPort)
+        {
+            byte[] bytesEnd = new byte[] { 0xff, 0xff, 0xff };
+            var timeReserv = DbConnection.GetDate();
+            if (timeReserv[0]<=DateTime.Now && timeReserv[1]>= DateTime.Now)
+            {
+                _serialPort.Write("t2.txt=\""+ timeReserv[0].ToString() + "\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t5.txt=\"" + timeReserv[1].ToString() + "\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+            }
+            else
+            {
+                _serialPort.Write("t2.txt=\"Нет\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t5.txt=\"Нет\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+            }
+        }
+
+
+        private static void addData(SerialPort _serialPort, string command)
+        {
+            byte[] bytesEnd = new byte[] { 0xff, 0xff, 0xff };
+            int day = int.Parse(command.Substring(command.IndexOf("addDate") + 7, 2));
+            int mouth = int.Parse(command.Substring(command.IndexOf("addDate") + 9, 2));
+            int year =int.Parse("20"+command.Substring(command.IndexOf("addDate") + 11, 2));
+            try
+            {
+                DateTime date = new DateTime(year, mouth, day);
+                _serialPort.Write("page AddTimePage");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t0.txt=\"" + day.ToString() + "\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t1.txt=\"" + mouth.ToString() + "\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t2.txt=\""+ year.ToString() +"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+
+            }
+            catch (Exception e)
+            {
+                _serialPort.Write("t0.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t1.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t2.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t3.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t4.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t5.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+            }
+            Console.WriteLine(day);
+            Console.WriteLine(mouth);
+            Console.WriteLine(year);
+        }
+
+        private static void addTime(SerialPort _serialPort, string command)
+        {
+            byte[] bytesEnd = new byte[] { 0xff, 0xff, 0xff };
+            string times = command.Substring(command.IndexOf("addTime") + 7);
+            string[] array = times.Split(' ');
+            int day = int.Parse(array[0]);
+            int mouth = int.Parse(array[1]);
+            int year = int.Parse(array[2]);
+            int hourStart = int.Parse(array[3]);
+            int minStart = int.Parse(array[4]);
+            int hourEnd = int.Parse(array[5]);
+            int minEnd = int.Parse(array[6]);
+
+            try
+            {
+                DateTime dateStart = new DateTime(year, mouth, day, hourStart, minStart, 00);
+                DateTime dateEnd = new DateTime(year, mouth, day, hourEnd, minEnd, 00);
+
+                DbConnection.AddTime(dateStart, dateEnd);
+                Console.WriteLine(dateStart);
+                Console.WriteLine(dateEnd);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                _serialPort.Write("t3.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t4.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t5.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t6.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t7.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t8.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t9.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+                _serialPort.Write("t10.txt=\"\"");
+                _serialPort.Write(bytesEnd, 0, bytesEnd.Length);
+            }
+        }
     }
 }
